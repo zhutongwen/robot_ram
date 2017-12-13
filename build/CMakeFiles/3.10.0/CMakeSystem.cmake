2a45354a3a516f9a1931021ffaeb3ac4a0dc18e8
z <576961736@qq.com>
2017-12-13 14:00:57 +0800

    ehtercat testing

--- build/CMakeFiles/3.10.0/CMakeSystem.cmake
+++ build/CMakeFiles/3.10.0/CMakeSystem.cmake
@@ -1,13 +1,13 @@
-set(CMAKE_HOST_SYSTEM "Linux-4.8.0-36-generic")
+set(CMAKE_HOST_SYSTEM "Linux-3.8.13-xenomai-2.6.4")
 set(CMAKE_HOST_SYSTEM_NAME "Linux")
-set(CMAKE_HOST_SYSTEM_VERSION "4.8.0-36-generic")
+set(CMAKE_HOST_SYSTEM_VERSION "3.8.13-xenomai-2.6.4")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
 
 
-set(CMAKE_SYSTEM "Linux-4.8.0-36-generic")
+set(CMAKE_SYSTEM "Linux-3.8.13-xenomai-2.6.4")
 set(CMAKE_SYSTEM_NAME "Linux")
-set(CMAKE_SYSTEM_VERSION "4.8.0-36-generic")
+set(CMAKE_SYSTEM_VERSION "3.8.13-xenomai-2.6.4")
 set(CMAKE_SYSTEM_PROCESSOR "x86_64")
 
 set(CMAKE_CROSSCOMPILING "FALSE")
---
+++ src/tinyxml2/tinyxml_test.cpp
@@ -0,0 +1,52 @@
+/*
+
+*/
+
+
+#include <iostream>
+#include <string>
+#include "tinyxml2.h"
+using namespace std;
+using namespace tinyxml2;
+void example1()
+{
+   XMLDocument doc;
+   if(doc.LoadFile("/home/z/work/ecat_master/resource/test.xml") != XML_SUCCESS)
+   {
+       cout << "load text.xml file fault!" << doc.LoadFile("test.xml") << endl;
+       exit(1);
+   }
+
+   XMLElement *root=doc.RootElement();
+   XMLElement *surface=root->FirstChildElement("node");
+   while (surface)
+   {
+       XMLElement *surfaceChild=surface->FirstChildElement();
+       const char* content;
+       const XMLAttribute *attributeOfSurface = surface->FirstAttribute();
+       cout<< attributeOfSurface->Name() << ":" << attributeOfSurface->Value() << endl;
+       while(surfaceChild)
+       {
+           content=surfaceChild->GetText();
+           surfaceChild=surfaceChild->NextSiblingElement();
+           cout<<content<<endl;
+       }
+       surface=surface->NextSiblingElement();
+   }
+}
+int main(int argc, char** argv)
+{
+   example1();
+   return 0;
+}
+
+//int main(int argc, char** argv)
+//{
+//    if (argc != 3)
+//    {
+//        cout << "usage: " << argv[0] << " <port> <ip>" << endl;
+//        exit(1);
+//    }
+
+//  return 0;
+//}
